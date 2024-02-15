d2dd516f100c18b1a06c2e910c61e0a41fee1a3a
Isaac Isvaldo Bunga <isaac.bunga@mamboo.co.ao>
2024-02-15 17:40:27 +0000

    feat:create useDTO

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {
+
+  }
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.prisma.user.create({data:createUserDto})
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany()
   }
 
   findOne(id: number) {
